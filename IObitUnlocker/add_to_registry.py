c03593c95ef691b06a5c767349a8c9953aed5f29
gmankab <gmankab@gmail.com>
2022-01-17 00:38:37 +0300

    aboba

--- IObitUnlocker/add_to_registry.py
+++ IObitUnlocker/add_to_registry.py
@@ -1,32 +1,48 @@
+import pathlib
+import os
+
+RUN_AFTER_SAVE = False
+
+unlocker_executable = 'IObitUnlockerPortable.exe'
+unlocker_full_path = os.path.abspath(unlocker_executable)
+
+context_menu_item = 'force delete'
+filename = 'force delete.reg'
 
 
-path = 'E:\\projects\\portable-soft\\IObitUnlocker\\IObitUnlockerPortable.exe'
+def main():
+    code = rf'''Windows Registry Editor Version 5.00
+
+    [HKEY_CLASSES_ROOT\*\shell\{context_menu_item}]
+    "Icon"="\"{unlocker_full_path}\""
+    "extended"=""
+
+    [HKEY_CLASSES_ROOT\*\shell\{context_menu_item}\command]
+    @="\"{unlocker_full_path}\" /delete \"%1\""
+
+    [HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item}]
+    "Icon"="\"{unlocker_full_path}\""
+    "extended"=""
+
+    [HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item}\command]
+    @="\"{unlocker_full_path}\" /delete \"%1\""
+
+    [HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item}]
+    "Icon"="\"{unlocker_full_path}\""
+    "extended"=""
+
+    [HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item}\command]
+    @="\"{unlocker_full_path}\""
+    '''
+
+    filedir = str(pathlib.Path(__file__).parent.resolve()).replace('\\', '/')
+    os.chdir(filedir)
+
+    file = open(filename, 'w')
+    file.write(code)
+
+    os.system(f'regedit.exe /S {filename}')
 
 
-code = rf'''
-Windows Registry Editor Version 5.00
-
-[HKEY_CLASSES_ROOT\*\shell\Уничтожить2]
-"Icon"="\"{path}\""
-"extended"=""
-
-[HKEY_CLASSES_ROOT\*\shell\Уничтожить2\command]
-@="\"{path}\" /delete \"%1\""
-
-[HKEY_CLASSES_ROOT\Directory\shell\Уничтожить2]
-"Icon"="\"{path}\""
-"extended"=""
-
-[HKEY_CLASSES_ROOT\Directory\shell\Уничтожить2\command]
-@="\"{path}\" /delete \"%1\""
-
-[HKEY_CLASSES_ROOT\Directory\background\shell\Уничтожить2]
-"Icon"="\"{path}\""
-"extended"=""
-
-[HKEY_CLASSES_ROOT\Directory\background\shell\Уничтожить2\command]
-@="\"{path}\""
-'''
-
-file = open('test.txt', 'w')
-file.write(code)
+if __name__ == '__main__':
+    main()
