c17c3a9bb433dffec3ff658a58df83262b2144ae
gmankab <gmankab@gmail.com>
2022-01-23 13:43:18 +0300

    aboba

--- IObitUnlocker/add_to_registry.py
+++ IObitUnlocker/add_to_registry.py
@@ -1,47 +1,56 @@
 import pathlib
 import os
 
+os.chdir(str(pathlib.Path(__file__).parent.resolve()).replace('\\', '/'))
+
+
 RUN_AFTER_SAVE = False
 
-unlocker_executable = 'IObitUnlockerPortable.exe'
-unlocker_full_path = os.path.abspath(unlocker_executable)
+context_menu_item_name = 'force delete'
 
-context_menu_item = 'force delete'
-filename = 'force delete.reg'
+unlocker_exe_name = 'IObitUnlockerPortable.exe'
+reg_filename = 'force delete.reg'
+
+
+unlocker_full_path = str(
+    pathlib.Path(unlocker_exe_name).resolve()
+).replace('\\', '\\\\')
+
+
+code = rf'''Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\*\shell\{context_menu_item_name}]
+"Icon"="\"{unlocker_full_path}\""
+"extended"=""
+
+[HKEY_CLASSES_ROOT\*\shell\{context_menu_item_name}\command]
+@="\"{unlocker_full_path}\" /delete \"%1\""
+
+[HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item_name}]
+"Icon"="\"{unlocker_full_path}\""
+"extended"=""
+
+[HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item_name}\command]
+@="\"{unlocker_full_path}\" /delete \"%1\""
+
+[HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item_name}]
+"Icon"="\"{unlocker_full_path}\""
+"extended"=""
+
+[HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item_name}\command]
+@="\"{unlocker_full_path}\""
+'''
 
 
 def main():
-    code = rf'''Windows Registry Editor Version 5.00
-
-    [HKEY_CLASSES_ROOT\*\shell\{context_menu_item}]
-    "Icon"="\"{unlocker_full_path}\""
-    "extended"=""
-
-    [HKEY_CLASSES_ROOT\*\shell\{context_menu_item}\command]
-    @="\"{unlocker_full_path}\" /delete \"%1\""
-
-    [HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item}]
-    "Icon"="\"{unlocker_full_path}\""
-    "extended"=""
-
-    [HKEY_CLASSES_ROOT\Directory\shell\{context_menu_item}\command]
-    @="\"{unlocker_full_path}\" /delete \"%1\""
-
-    [HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item}]
-    "Icon"="\"{unlocker_full_path}\""
-    "extended"=""
-
-    [HKEY_CLASSES_ROOT\Directory\background\shell\{context_menu_item}\command]
-    @="\"{unlocker_full_path}\""
-    '''
-
-    filedir = str(pathlib.Path(__file__).parent.resolve()).replace('\\', '/')
-    os.chdir(filedir)
-
-    file = open(filename, 'w')
+    file = open(reg_filename, 'w')
     file.write(code)
 
-    os.system(f'regedit.exe /S {filename}')
+    reg_full_path = os.path.abspath(reg_filename)
+
+    if RUN_AFTER_SAVE:
+        os.system(f'regedit.exe /S "{reg_full_path}"')
+        print(f'regedit.exe /S "{reg_full_path}"')
 
 
 if __name__ == '__main__':
